51cf3b5ac0796c508af7ef354d4ce8d021d80af6
向前Hell <542168513@qq.com>
2021-03-18 20:41:05 +0800

    [feat] update-deploy-sh

--- deploy.sh
+++ deploy.sh
@@ -15,10 +15,8 @@ fi
 git config --local user.name "向前Hell"
 git config --local user.email "542168513@qq.com"
 
-export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
-
 # 如果部署到 https://{USERNAME}.github.io
-git push origin master
+git push -f git@github.com:maxlxq/maxlxq.github.io.git master:master
 
 # 如果部署到 https://{USERNAME}.github.io/{REPO}
 #git push -f git@github.com:maxlxq/maxlxq.github.io.git master
